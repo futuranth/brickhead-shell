f1bae6c5c64d64122e1996bd9d02462077a108ca
futuranth <79914142+futuranth@users.noreply.github.com>
2024-03-06 20:28:30 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,28 @@
+#include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 int main(int argc, char * argv[]) {
-	char * command;
-	scanf("%s", command);
-	pid_t child = fork();
-	char * child_argv[] = {"NULL"};
-	if(child == 0) {
-		execv(command, child_argv);
-		exit(127);
-	} else waitpid(child, 0, 0);
+	while(true) {
+		char * restrict command;
+		printf("bsh $ ");
+		scanf("%s", command);
+		if(feof(stdin) || ! strcmp(command, "exit")) break;
+		pid_t child = fork();
+		char * child_argv[] = {command};
+		if(child == 0) {
+			execv(command, child_argv);
+		} else waitpid(child, 0, 0);
+		if(errno) {
+			perror("Error in bsh");
+			continue;
+		}
+	}
+	printf("Exit\n");
 	return 0;
 }
